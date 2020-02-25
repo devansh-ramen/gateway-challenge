1c244875a3d3059a6e0de1e2e5b8896a458a96a0
devanshramen <devansh.ramen@telecom.mu>
2020-02-25 23:26:44 +0400

    Move codes to github

---
+++ PaymentGateway.test/PaymentItemValidatorTest.cs
@@ -0,0 +1,53 @@
+using System;
+using Xunit;
+using PaymentGateway.Controllers;
+
+namespace PaymentGateway.test
+{
+    public class PaymentItemValidatorTest
+    {
+        [Fact]
+        public void testValidCreditCard()
+        {
+            string creditCardNumber = "4512-4567-4567-4567";
+            string expiryDate = "11/2022";
+            string cvv = "111";
+
+            bool isValid = PaymentItemValidator.IsCreditCardInfoValid(creditCardNumber, expiryDate, cvv);
+
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void testInvalidCreditCard()
+        {
+            string creditCardNumber = "4512-4567-4567-4567";
+            string expiryDate = "11/2019";
+            string cvv = "111";
+
+            bool isValid = PaymentItemValidator.IsCreditCardInfoValid(creditCardNumber, expiryDate, cvv);
+
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void testValidAmount()
+        {
+            float amount = 100;
+
+            bool isValid = PaymentItemValidator.IsAmountValid(amount);
+
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void testInvalidAmount()
+        {
+            float amount = -10;
+
+            bool isValid = PaymentItemValidator.IsAmountValid(amount);
+
+            Assert.False(isValid);
+        }
+    }
+}
---
+++ PaymentGateway/Startup.cs
@@ -0,0 +1,108 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PaymentGateway.Models;
+
+namespace PaymentGateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<PaymentContext>(opt =>
+              opt.UseInMemoryDatabase("PaymentItem"));
+
+            services.AddDbContext<MerchantContext>(opt =>
+               opt.UseInMemoryDatabase("Merchant"));
+
+            //services.AddDbContext<PaymentContext>(options =>
+            //     options.UseSqlite(Configuration.GetConnectionString("GatewayDB")));
+
+            //services.AddDbContext<MerchantContext>(options =>
+            //     options.UseSqlite(Configuration.GetConnectionString("GatewayDB")));
+
+
+            services.AddControllersWithViews();
+            services.AddControllers();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
+
+            services.AddSwaggerDocument(config => config.PostProcess = document =>
+            {
+                document.Info.Version = "v1";
+                document.Info.Title = "Payment Gateway Challenge";
+                document.Info.Description = "REST APIs for communicating with Payment Gateway.";
+                document.Info.TermsOfService = "None";
+                document.Info.Contact = new NSwag.OpenApiContact
+                {
+                    Name = "Devansh Ramen",
+                    Email = string.Empty,
+                };
+                document.Info.License = new NSwag.OpenApiLicense
+                {
+                };
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+
+            // Register the Swagger generator and the Swagger UI middlewares
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
